5a7afee9b28f878cb35c1da29d81ac3c14b63dd5
Mohamed Ahmed <mohamed.ahmed@s3-id.com>
2025-07-14 16:33:10 +0100

    Test the button design

---
+++ UIBaseClass/Controls/StyleCommands/RoundButton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace UIBaseClass.Controls.StyleCommands
+{
+    public class RoundButton : Button
+    {
+        // Register a CornerRadius dependency property
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register(
+                nameof(CornerRadius),
+                typeof(CornerRadius),
+                typeof(RoundButton),
+                new FrameworkPropertyMetadata(new CornerRadius(0), FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public CornerRadius CornerRadius
+        {
+            get => (CornerRadius)GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
+        }
+
+        static RoundButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(RoundButton), new FrameworkPropertyMetadata(typeof(RoundButton)));
+        }
+    }
+}
---
+++ UIBaseClass/Styles/Butttons/CustomButton.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UIBaseClass.Controls.StyleCommands;
+
+namespace UIBaseClass.Styles.Butttons
+{
+    /// <summary>
+    /// Interaction logic for CustomButton.xaml
+    /// </summary>
+    public partial class CustomButton : UserControl
+    {
+        public static readonly DependencyProperty CornerRadiusProperty =
+    DependencyProperty.Register(
+        nameof(CornerRadius),
+        typeof(CornerRadius),
+        typeof(RoundButton),
+        new FrameworkPropertyMetadata(new CornerRadius(0), FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public CornerRadius CornerRadius
+        {
+            get => (CornerRadius)GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
+        }
+
+        public CustomButton()
+        {
+            InitializeComponent();
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(RoundButton), new FrameworkPropertyMetadata(typeof(RoundButton)));
+        }
+    }
+}
