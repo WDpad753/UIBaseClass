cb16907b3b3bc2868de787be3d623b656bba6b9b
Mohamed Ahmed <mohamed.ahmed@s3-id.com>
2025-07-17 08:52:32 +0100

    Modified some Base Classes and added some new files.

---
+++ UIBaseClass/Services/Dialog/DialogService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IDialogService = UIBaseClass.Services.Dialog.Interface.IDialogService;
+
+namespace UIBaseClass.Services.Dialog
+{
+    public class DialogService : IDialogService
+    {
+        private readonly IContainerExtension _container;
+
+        public DialogService(IContainerExtension container)
+        {
+            _container = container;
+        }
+
+        public void ShowDialog(string name, IDialogParameters parameters, DialogCallback callback)
+        {
+            var dialogWindow = CreateDialogWindow();
+            var content = _container.Resolve<object>(name);
+
+            if (content is not IDialogAware viewModel)
+                throw new NullReferenceException("Dialog content must implement IDialogAware.");
+
+            dialogWindow.DataContext = viewModel;
+            viewModel.OnDialogOpened(parameters);
+
+            dialogWindow.Closed += (o, e) =>
+            {
+                callback.Invoke(new DialogResult(ButtonResult.OK));
+            };
+
+            dialogWindow.ShowDialog();
+        }
+
+        private IDialogWindow CreateDialogWindow()
+        {
+            var window = new DialogWindow();
+            return window;
+        }
+    }
+}
---
+++ UIBaseClass/Services/Dialog/Interface/IDialogService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IDialogservice = Prism.Dialogs.IDialogService;
+
+namespace UIBaseClass.Services.Dialog.Interface
+{
+    public interface IDialogService : IDialogservice
+    {
+        void ShowDialog(string name, IDialogParameters parameters, DialogCallback callback);
+    }
+}
